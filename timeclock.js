ede76bda6a59de2308c36641cdb4dba6b6242a5c
Kyle Tucker <kyleatucker@gmail.com>
2017-02-03 08:10:43 -0500

    moar implementation

--- timeclock.js
+++ timeclock.js
@@ -12,7 +12,7 @@ const FILENAME = '_timeclock.csv'
 
 // Set the number of MINUTES of inactivity allowed before the timeclock
 // automatically stops
-const TIMEOUT = 1
+const TIMEOUT = 30
 
 let state = {
   path: null,
@@ -45,11 +45,22 @@ class App {
     args.forEach((msg) => {
       process.stdout.write(msg)
     })
+
+    return this
   }
 
   _writeln() {
     this._write.apply(this, arguments)
     process.stdout.write('\n')
+
+    return this
+  }
+
+  _clear() {
+    process.stdout.clearLine()
+    process.stdout.cursorTo(0)
+
+    return this
   }
 
   _showPrompt(defaultName) {
@@ -70,45 +81,96 @@ class App {
   _wait() {
     clearTimeout(this.waitTimeout)
     setTimeout(() => {
-      this._writeln(chalk.yellow('\nStopping timeclock due to inactivity.'))
+      this._writeln(' > ', chalk.yellow('Stopping timeclock due to inactivity.'))
       this.finish()
     }, TIMEOUT * 60000)
+  }
+
+  _roundMinutes(date, nearest, midpoint) {
+    let actualMinutes = date.getMinutes()
+    let remainder = actualMinutes % nearest
+    let diff = nearest - remainder
+    let rounded = remainder > midpoint
+      ? actualMinutes + diff
+      : actualMinutes - remainder
+
+    date.setMinutes(rounded, 0, 0)
+
+    return this
+  }
+
+  _saveCsv() {
+    let fileStream = fs.createWriteStream(this.state.filePath, { flags: 'a' })
+    let csvStream = csv()
+
+    csvStream.transform((data) => {
+      console.log(data)
+      return [ 'a', 'b', 'c' ]
+    })
+    console.log('writin stuff')
+
+    fileStream.on('finish', () => {
+      console.log('dookie')
+    })
+    csvStream.on('finish', () => {
+      console.log('aww fuck yeah, all done with the csv')
+      fileStream.end()
+    })
+
+    csvStream.pipe(fileStream)
+    // csvStream.write(['one', 'two', 'three'])
+    csvStream.end()
   }
 
   init() {
     last.read(this.state.filePath, 1)
       .then((line) => {
-        csv.fromString(line)
-          .on('data', (data) => {
-            this._showPrompt(data[2])
-          })
+        let csvStream = csv.fromString(line)
+        let defaultName = ''
+        csvStream.on('data', (data) => {
+          defaultName = data[2]
+          csvStream.end()
+        })
+        this._showPrompt(defaultName)
       }, (error) => {
         this._showPrompt('')
       })
   }
 
   run() {
+    this
+      ._writeln(' > ', chalk.green(`You're on the clock...`))
+      ._write(' > ')
+
     this._wait()
-    chokidar.watch(this.state.path, (e, p) => {
-      console.log(e, p)
+    chokidar.watch(`${this.state.path}/**/*`, {
+      ignoreInitial: true
+    }).on('all', (event, path) => {
+      let relPath = path.replace(`${this.state.path}/`, '')
+      this
+        ._clear()
+        ._write(' > Latest change: ', chalk.magenta(relPath))
       this._wait()
     })
 
     process.on('SIGINT', () => {
-      this._writeln(chalk.yellow('\nStopping timeclock manually.'))
+      this._writeln('\n > ', chalk.yellow('Clocking out manually.'))
       this.finish()
+
+      return false
     })
   }
 
   finish() {
     this.state.data.end = new Date()
-    let _d = this.state.data.end
 
-    console.log(`Before:`, chalk.green(_d))
+    // Round start/end to previous/next 15 minutes
+    this
+      ._roundMinutes(this.state.data.start, 15, 10)
+      ._roundMinutes(this.state.data.end, 15, 5)
 
-    // 8:00 -> 8:00
-    // 8:01 -> 8:00
-    // 8:05 -> 8:15
+    this._saveCsv()
+
     process.exit()
   }
 }
